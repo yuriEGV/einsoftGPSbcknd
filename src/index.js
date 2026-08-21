a3392110626f6a5358b2ba1c4cbb3dbd3eb3e44d
yuriEGV <yguajardov@gmail.com>
2026-08-21 09:03:23 -0400

    fix(cors): fix Vercel preflight CORS credentials headers

--- src/index.js
+++ src/index.js
@@ -51,11 +51,15 @@ app.set('io', io);
 
 // Universal CORS Header Middleware for Vercel
 app.use((req, res, next) => {
-  const origin = req.headers.origin || '*';
-  res.setHeader('Access-Control-Allow-Origin', origin);
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  const origin = req.headers.origin;
+  if (origin) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
